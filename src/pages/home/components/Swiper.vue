da4ad14f206f5333aa10edd17a5190c1e0470762
wenjun615 <835045686@qq.com>
2020-03-05 20:06:25 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,17 +1,4 @@
 <template>
-<!--  <div class="wrapper">
-    <swiper :options="swiperOption">
-      <swiper-slide>
-        <img class="swiper-img"
-             src="https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/4652ae60c4b41658f270219335ef750a.JPG_890x330_31012fb8.jpg"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img"
-             src="https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/86ca4dc2777b997607e97eb8471dcafd.jpg_890x330_0eee7728.jpg"/>
-      </swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
-  </div>-->
   <div class="wrapper">
     <swiper :options="swiperOption">
       <swiper-slide v-for="item of swiperList" :key="item.id">
