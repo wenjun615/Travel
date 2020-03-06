946f978989b33681dbccdc0d11a43dc8e4014a5b
wenjun615 <835045686@qq.com>
2020-03-06 20:48:16 +0800

    add city router.

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,16 +7,21 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      城市
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeHeader'
+  name: 'HomeHeader',
+  props: {
+    city: String
+  }
 }
 </script>
 
@@ -25,15 +30,18 @@ export default {
 
   .header
     display flex
-    line-height .86rem
+    line-height $headerHeight
     background $bgColor
     color #fff
+
     .header-left
       width .64rem
       float left
+
       .back-icon
         text-align center
         font-size .4rem
+
     .header-input
       flex 1
       height .64rem
@@ -44,10 +52,13 @@ export default {
       background #fff
       border-radius .1rem
       color #ccc
+
     .header-right
       width 1.24rem
       float right
       text-align center
+      color #fff
+
       .arrow-icon
         font-size .24rem
         margin-left -.04rem
