de1626f2411e4b54e2067682aca0286541557bfe
wenjun615 <835045686@qq.com>
2020-03-08 02:38:26 +0800

    detail ajax.

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img"
-        src="http://img1.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_600x330_1abc2773.jpg">
+      <img class="banner-img" :src="bannerImg"/>
       <div class="banner-info">
-        <div class="banner-title">北京野生动物园(AAAA景区)</div>
-        <div class="banner-number"><span class="iconfont banner-icon">&#xe629;</span>39</div>
+        <div class="banner-tittle">
+          {{this.sightName}}
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe629;</span>
+          {{this.bannerImgs.length}}
+        </div>
       </div>
     </div>
-    <common-gallary :imgs="imgs" v-if="showGallary" @close="handleGallaryClose"></common-gallary>
   </div>
 </template>
 
@@ -17,6 +20,11 @@ import CommonGallary from 'common/gallary/Gallary'
 
 export default {
   name: 'DetailBanner',
+  props: {
+    sightName: String,
+    bannerImg: String,
+    bannerImgs: Array
+  },
   data () {
     return {
       showGallary: false,
@@ -46,8 +54,10 @@ export default {
     overflow hidden
     height 0
     padding-bottom 55%
+
     .banner-img
       width 100%
+
     .banner-info
       display flex
       position absolute
@@ -57,10 +67,12 @@ export default {
       line-height .6rem
       color #fff
       background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+
       .banner-title
         flex 1
         font-size .32rem
         padding 0 .2rem
+
       .banner-number
         height .32rem
         line-height .32rem
@@ -69,6 +81,7 @@ export default {
         border-radius .2rem
         background rgba(0, 0, 0, .8)
         font-size .24rem
+
         .banner-icon
           font-size .24rem
 </style>
